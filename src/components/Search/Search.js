a1b9ea89a8715c634d15ae7151aa07a28551a77e
bohong <977106024@qq.com>
2019-05-25 00:23:05 +0800

    update SearchFrom

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -7,6 +7,15 @@ class Search extends React.Component{
         expand: false,
     }
 
+    aaa(rule, value, callback){
+        let reg = /^[\d]+$/;
+        if(!reg.test(value)){
+            callback('只能输入数字！')
+            return
+        }
+        callback()
+    }
+
     // To generate mock Form.Item
     getFields() {
         const criteria = this.props.criteria
@@ -15,54 +24,55 @@ class Search extends React.Component{
         const children = [];
         for (let i = 0; i < criteria.length; i++) {
             children.push(
-                <Col span={8} key={i} style={{ display: i < count ? 'block' : 'none' }}>
-                    <Form.Item label={`${criteria[i]}`}>
-                        {getFieldDecorator(`${criteria[i]}`, {
-                            rules: [
-                                {
-                                    type: "number",
-                                    message: 'id必须为数组!',
-                                },
-                            ],
-                        })(<Input placeholder={`请输入${criteria[i]}`} />)}
-                    </Form.Item>
-                </Col>,
+            <Col span={8} key={i} style={{ display: i < count ? 'block' : 'none' }}>
+        <Form.Item label={`${criteria[i]}`}>
+            {getFieldDecorator(`${criteria[i]}`, {
+                rules: [{
+
+                },
+                    {
+                        validator:this.aaa.bind(this)
+                    }
+                ],
+            })(<Input placeholder={`请输入${criteria[i]}`} />)}
+            </Form.Item>
+            </Col>,
             );
+            }
+            return children;
         }
-        return children;
-    }
 
-    handleSearch = e => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            console.log('Received values of form: ', values);
+        handleSearch = e => {
+            e.preventDefault();
+            this.props.form.validateFields((err, values) => {
+                console.log('Received values of form: ', values);
         });
-    };
+        };
 
-    handleReset = () => {
-        this.props.form.resetFields();
-    };
+        handleReset = () => {
+            this.props.form.resetFields();
+        };
 
 
-    render(){
-        return (
-            <Form className="ant-advanced-search-form" onSubmit={this.handleSearch}>
+        render(){
+            return (
+                <Form className="ant-advanced-search-form" onSubmit={this.handleSearch}>
                 <Row gutter={24}>{this.getFields()}</Row>
                 <Row>
-                    <Col span={24} style={{ textAlign: 'right' }}>
-                        <Button type="primary" htmlType="submit">
-                            Search
-                        </Button>
-                        <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
-                            Clear
-                        </Button>
-                    </Col>
+                <Col span={24} style={{ textAlign: 'right' }}>
+        <Button type="primary" htmlType="submit">
+                Search
+                </Button>
+                <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
+                Clear
+                </Button>
+                </Col>
                 </Row>
-            </Form>
+                </Form>
         )
+        }
     }
-}
 
-const SearchForm = Form.create({ name: 'advanced_search' })(Search);
+    const SearchForm = Form.create({ name: 'advanced_search' })(Search);
 
-export default SearchForm
+    export default SearchForm
